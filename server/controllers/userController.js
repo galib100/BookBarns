be053b74e8d8390feb2c050028e1b72f36d8c13e
Musanna Galib <mgalib101@gmail.com>
2022-03-19 15:31:45 +0600

    log in part

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,41 +6,47 @@ const { set } = require('mongoose')
 
 
 
-const nameCont = (req,res)=>{
+const nameCont = (req, res) => {
     res.send('my name is galib');
 }
 
 
 const postSignupController = (req, res) => {
+    var errMsg = "";
+    req.body.password != req.body.password1 ? errMsg = "Password must be same " : errMsg = "pass matching ok"
     bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
+        if (err && errMsg.length > 1) {
             console.log(err)
+            console.log(errMsg)
+            res.send(errMsg);
+
+        } else {
+
+            let user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                number: req.body.number,
+                password: hash,
+                password1: hash
+            });
+
+
+            try {
+                const savedUser = user.save();
+                console.log(savedUser);
+                res.send(savedUser);
+            } catch (error) {
+                res.status(400).send(error)
+            }
         }
 
-        let user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            number: req.body.number,
-            password: hash,
-            password1: hash
-        })
 
-        user.save()
-            .then(result => {
-                console.log("register successfully!!")
-                console.log(result)
-                 
-            })
-            .catch(err => {
-                res.json({
-                    err
-                })
-            })
+
     })
 }
 
 
 module.exports = {
-nameCont,postSignupController
+    nameCont, postSignupController
 
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,5 +3,7 @@ const { nameCont ,postSignupController} = require('../controllers/userController
 const routes = express.Router();
 
 routes.get('/name',nameCont);
+//localhost:5000/api/signup
+
 routes.post('/signup',postSignupController)
 module.exports = routes
